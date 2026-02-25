b4d2e4164350d3710e139c54fe3820d57a1a5784
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-26 00:32:49 +0600

    ES6 intro: Diffenrece berween var, let, const

---
+++ let-const.js
@@ -0,0 +1,30 @@
+const name = "Bangladesh";
+// name = "Singapore";
+
+const countryName = "BD_" + name;
+console.log(countryName);
+
+let price = 120;
+price = 150;
+console.log(price);
+
+const dress = ["dress", "Pant", "Shoe"];
+// dress = ["dress", "Pant", "Shoe", "Belt"]
+dress.push("Belt");
+
+console.log(dress);
+
+const student = {
+  name: "John",
+  age: 25,
+};
+
+// student = {
+//   name: "John",
+//   age: 25,
+//   location: "london",
+// };
+
+student.name = "Jane";
+student.age  = 30;
+console.log(student)
